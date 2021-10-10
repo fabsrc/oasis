c20e3ff4591d445cad05191a392b0bc14507720c
Fabian Schneider <fabbbbbi+git@googlemail.com>
2021-10-10 16:23:51 +0200

    :sparkles: Add worktop, add /logout endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,113 @@
-import { handle } from './handler'
+import { Router, listen } from 'worktop'
+import type { KV } from 'worktop/kv'
+import {
+  createSession,
+  deleteSession,
+  getSession,
+  SESSION_COOKIE_NAME,
+} from './session'
 
-addEventListener('fetch', (event) => {
-  event.respondWith(handle(event.request))
+declare const GITHUB_CLIENT_ID: string
+declare const GITHUB_CLIENT_SECRET: string
+declare const KV_USERS: KV.Namespace
+
+const API = new Router()
+
+API.add('GET', '/login', (_request, response) => {
+  response.send(302, null, {
+    location: `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&redirect_uri=http://localhost:8787/callback`,
+  })
 })
+
+API.add('GET', '/logout', async (request, response) => {
+  await deleteSession(request)
+  return response.send(302, null, {
+    location: '/',
+    'set-cookie':
+      `${SESSION_COOKIE_NAME}=; Expires=Thu, 01 Jan 1970 00:00:00 GMT`,
+  })
+})
+
+API.add('GET', '/callback', async (request, response) => {
+  try {
+    const code = request.query.get('code')
+
+    if (!code) {
+      throw new Error('code param missing')
+    }
+
+    const tokenResponse = await fetch(
+      'https://github.com/login/oauth/access_token',
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          accept: 'application/json',
+          'user-agent': 'OASIS on Cloudflare Workers',
+        },
+        body: JSON.stringify({
+          client_id: GITHUB_CLIENT_ID,
+          client_secret: GITHUB_CLIENT_SECRET,
+          code,
+        }),
+      },
+    )
+    const tokenResult = await tokenResponse.json()
+
+    if (tokenResult.error) {
+      response.send(401, JSON.stringify(tokenResult))
+      return
+    }
+
+    const userResponse = await fetch('https://api.github.com/user', {
+      method: 'GET',
+      headers: {
+        authorization: `token ${tokenResult.access_token}`,
+        'user-agent': 'OASIS on Cloudflare Workers',
+      },
+    })
+
+    if (!userResponse.ok) {
+      throw new Error(await userResponse.text())
+    }
+
+    const userResult = await userResponse.json()
+
+    const { id, login } = userResult
+    await KV_USERS.put(id, JSON.stringify({ id, login }))
+
+    const sessionId = await createSession({ userId: id })
+
+    response.send(302, null, {
+      'set-cookie': `${SESSION_COOKIE_NAME}=${sessionId}`,
+      location: '/',
+    })
+    return
+  } catch (error) {
+    console.error(error)
+    response.send(500, (error as Error).message)
+  }
+})
+
+API.add('GET', '/', async (request, response) => {
+  const [sessionId, session] = await getSession(request)
+
+  if (sessionId) {
+    if (session) {
+      const user = await KV_USERS.get<{ login: string }>(session.userId, {
+        type: 'json',
+        cacheTtl: 3600,
+      })
+
+      if (user) {
+        return response.send(200, { hello: user.login })
+      }
+    } else {
+      return response.send(302, null, { location: '/login' })
+    }
+  }
+
+  return response.send(200, { hello: 'world' })
+})
+
+listen(API.run)
---
+++ src/session.ts
@@ -0,0 +1,50 @@
+import { parse } from 'worktop/cookie'
+import { KV } from 'worktop/kv'
+import { ServerRequest } from 'worktop/request'
+import { uuid } from 'worktop/utils'
+
+declare const KV_SESSIONS: KV.Namespace
+
+interface SessionData {
+  userId: string
+}
+
+export const SESSION_COOKIE_NAME = 'oasis-session'
+
+export const createSession = async (data: SessionData): Promise<string> => {
+  const sessionId = await uuid()
+
+  await KV_SESSIONS.put(sessionId, JSON.stringify(data), {
+    expirationTtl: 3600,
+  })
+
+  return sessionId
+}
+
+export const getSession = async (
+  request: ServerRequest,
+): Promise<[string | null, SessionData | null]> => {
+  const cookie = parse(request.headers.get('Cookie') || '')
+  const sessionId = cookie[SESSION_COOKIE_NAME]
+
+  if (sessionId) {
+    const session = await KV_SESSIONS.get<SessionData>(sessionId, {
+      type: 'json',
+    })
+    if (session) {
+      return [sessionId, session]
+    }
+
+    return [sessionId, null]
+  }
+
+  return [null, null]
+}
+
+export const deleteSession = async (request: ServerRequest): Promise<void> => {
+  const [sessionId] = await getSession(request)
+
+  if (sessionId) {
+    await KV_SESSIONS.delete(sessionId)
+  }
+}
